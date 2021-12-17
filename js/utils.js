04e59ee5645cf86992a7764cec9fd8287a95a75f
mikero204 <99407005@stud.sju.edu.tw>
2021-12-17 16:18:10 +0800

    1217

---
+++ js/utils.js
@@ -0,0 +1,25 @@
+let utils = {};
+
+utils.getOffset = function (element) {
+  let mouse = { x: 0, y: 0 };
+  element.addEventListener("mousemove", function (e) {
+    let { x, y } = utils.eventWrapper(e);
+    mouse.x = x;
+    mouse.y = y;
+  });
+  return mouse;
+};
+
+utils.eventWrapper = function (ev) {
+  let { pageX, pageY, target } = ev;
+  let { left, top } = target.getBoundingClientRect();
+  return { x: pageX - left, y: pageY - top };
+};
+
+utils.toRad = function (radius) {
+  return (radius * Math.PI) / 180;
+};
+
+utils.toAngle = function (rad) {
+  return (rad * 180) / Math.PI;
+};
